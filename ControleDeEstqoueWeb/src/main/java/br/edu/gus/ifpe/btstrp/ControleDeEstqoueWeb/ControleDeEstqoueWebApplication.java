b7e1d65bd9a63c2638471d2cc57ae10b4872d4ef
Gênesis Lima <profgenesislima@gmail.com>
2021-06-15 14:53:44 -0300

    initial commit for Controle de Estoque Web Service Aula IFPE.

---
+++ ControleDeEstqoueWeb/src/main/java/br/edu/gus/ifpe/btstrp/ControleDeEstqoueWeb/ControleDeEstqoueWebApplication.java
@@ -0,0 +1,13 @@
+package br.edu.gus.ifpe.btstrp.ControleDeEstqoueWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeEstqoueWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeEstqoueWebApplication.class, args);
+	}
+
+}
---
+++ ControleDeEstqoueWeb/src/test/java/br/edu/gus/ifpe/btstrp/ControleDeEstqoueWeb/ControleDeEstqoueWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.gus.ifpe.btstrp.ControleDeEstqoueWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeEstqoueWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
